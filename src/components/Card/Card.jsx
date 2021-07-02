479e81ccefa8f56187995bf2cc3881288553e611
Cameron Romo <cromo1225@gmail.com>
2021-07-02 14:49:00 -0600

    Invoke prop to delete a card when button is clicked

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 import './Card.css';
 
-const Card = ({ name, date, time, number }) => {
+const Card = ({ id, name, date, time, number, onDelete }) => {
+
   return ( 
     <article className='card'>
       <h3>Name: { name }</h3>
       <p>Date: { date }</p>
       <p>Reservation Time: { time }</p>
       <p>Guests: { number }</p>
+      <button onClick={ () => onDelete(id) } className='delete-reservation-btn' >Delete Reservation</button>
     </article>
    );
 }
