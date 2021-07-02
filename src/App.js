5cabc0a0cdae1e742fd4cb91ef3a81e87bce202a
Cameron Romo <cromo1225@gmail.com>
2021-07-02 15:54:25 -0600

    Remove link to use in reservations component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Route, Redirect, Switch, Link } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 import './App.css';
 import ReservationForm from './components/ReservationForm/ReservationForm';
 import Reservations from './components/Reservations/Reservations';
 
 const App = () => {
+
   return ( 
     <React.Fragment>
       <main className='App'>
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <Link to='/reservation-form' className='add-reservation-btn' >Add a reservation</Link>
         <Switch>
           <Route path='/reservation-form' component={ ReservationForm }/>
           <Route path='/reservations' component={ Reservations } />
