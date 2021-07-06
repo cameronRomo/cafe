061f4fef0dc7a6c77e5111afc9bf0e63cb35fcc7
Cameron Romo <cromo1225@gmail.com>
2021-07-06 17:31:23 -0600

    Add rendering for pagenation, errors with finding pathing

--- src/components/Reservations/Reservations.jsx
+++ src/components/Reservations/Reservations.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { Link, Route } from 'react-router-dom';
+import _ from 'lodash';
 
 import './Reservations.css';
 import { getReservationData, deleteReservation } from '../../services/apiCalls';
 import Card from '../Card/Card';
 import ReservationForm from '../ReservationForm/ReservationForm';
+import { paginate } from '../../utils/paginate';
+import Pagination from '../common/Pagination';
+import ReservationsTable from '../ReservationsTable/ReservationsTable';
 
 class Reservations extends Component {
   constructor() {
     super();
     this.state = { 
       upcomingReservations: [],
+      currentPage: 1,
+      pageSize: 4,
      }
   }
 
@@ -26,7 +32,43 @@ class Reservations extends Component {
     await deleteReservation(deprecatedReservationId);
   }
 
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page })
+  }
+
+  handleSort = sortColumn => {
+    this.setState({ sortColumn })
+  }
+
+  getPageData = () => {
+    const {
+      pageSize,
+      currentPage,
+      sortColumn,
+      reservations: allReservations
+    } = this.state;
+
+    const reservations = paginate(currentPage, pageSize)
+
+    return { 
+      totalCount: reservations.length,
+      data: reservations
+    }    
+  }
+
   render() { 
+
+    const { length: count} = this.state.upcomingReservations;
+    const {
+      pageSize,
+      currentPage,
+      sortColumn
+    } = this.state;
+
+    if (count === 0) return <p>There are currently no reservations in the database.</p>
+
+    const { totalCount, data: reservations } = this.getPageData()
+
     const reservationCards = this.state.upcomingReservations.map(reservation => {
       return (
         <Card 
@@ -44,9 +86,20 @@ class Reservations extends Component {
       <>
         <Link to='/reservation-form' className='add-reservation-btn' >Add a reservation</Link>
         <Route path='/reservation-form' component={ ReservationForm }/>
-        <section className='reservations-container' >
+        {/* <section className='reservations-container' >
           { reservationCards }
-        </section>
+        </section> */}
+        <ReservationsTable 
+          reservations={ reservations }
+          onSort={ this.handleSort }
+          sortColumn={ sortColumn }
+        />
+        <Pagination 
+          itemsCount={ totalCount }
+          pageSize={ pageSize }
+          onPageChange={ this.handlePageChange }
+          currentPage={ currentPage }
+        />
       </>
     );
   }
