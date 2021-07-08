80aba0808168f3ef9d22dc6a172430897518c7e4
Cameron Romo <cromo1225@gmail.com>
2021-07-07 18:32:21 -0600

    Change class name from card to reservation-card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,13 +5,15 @@ import './Card.css';
 const Card = ({ id, name, date, time, number, onDelete }) => {
 
   return ( 
-    <article className='card'>
-      <h3>Name: { name }</h3>
-      <p>Date: { date }</p>
-      <p>Reservation Time: { time }</p>
-      <p>Guests: { number }</p>
-      <button onClick={ () => onDelete(id) } className='delete-reservation-btn' >Delete Reservation</button>
-    </article>
+    <div className='container'>
+      <article className='reservation-card'>
+        <h3 className='card-name'>Name: { name }</h3>
+        <p>Date: { date }</p>
+        <p>Reservation Time: { time }</p>
+        <p>Guests: { number }</p>
+        <button onClick={ () => onDelete(id) } className='delete-reservation-btn' >Delete Reservation</button>
+      </article>
+    </div>
    );
 }
  
