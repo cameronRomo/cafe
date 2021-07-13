5fe9e748e6230717319c874f22747197a009aa7f
Cameron Romo <cromo1225@gmail.com>
2021-07-13 14:48:46 -0600

    Test rendering of app title

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <App/>
+      </MemoryRouter>
+    )
+  })
+
+  it('Should render "Turing Cafe Reservations" title', () => {
+    const header = screen.getByRole('heading', {
+      name: /turing cafe reservations/i
+    })
+    expect(header).toBeInTheDocument();
+  })
+})
